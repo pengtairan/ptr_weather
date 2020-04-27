97608ca080ef9d9617b3b75e82288946c7b48e04
Peng Tairan <1324305357@qq.com>
2020-04-27 15:18:46 +0800

    2.加入创建数据库和表的各项配置

---
+++ app/src/main/java/db/City.java
@@ -0,0 +1,42 @@
+package db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int proviceId;
+
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProviceId() {
+        return proviceId;
+    }
+    public void setProviceId(int proviceId) {
+        this.proviceId = proviceId;
+    }
+
+
+}
